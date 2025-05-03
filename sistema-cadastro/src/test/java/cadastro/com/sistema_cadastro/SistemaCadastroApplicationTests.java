62825ace2bdca2b379af48484c86bceb2d4c9e2b
Mayara Sampaio <mayarasampaio.dev@gmail.com>
2025-05-03 13:27:33 +0100

    first commit

---
+++ sistema-cadastro/src/test/java/cadastro/com/sistema_cadastro/SistemaCadastroApplicationTests.java
@@ -0,0 +1,13 @@
+package cadastro.com.sistema_cadastro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaCadastroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
