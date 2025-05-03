2f821d235249c8afe53af60516a99b98d142ebb4
Mayara Sampaio <mayarasampaio.dev@gmail.com>
2025-05-03 13:53:54 +0100

    feat(service): asdd SistemaDeEstoque

--- sistema-cadastro/src/main/java/cadastro/com/sistema_cadastro/SistemaCadastroApplication.java
+++ sistema-cadastro/src/main/java/cadastro/com/sistema_cadastro/SistemaCadastroApplication.java
@@ -1,13 +1,33 @@
 package cadastro.com.sistema_cadastro;
 
+import cadastro.com.sistema_cadastro.entities.Produto;
+import cadastro.com.sistema_cadastro.services.SistemasDeEstoque;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SistemaCadastroApplication {
+public class SistemaCadastroApplication implements CommandLineRunner {
+
+	@Autowired
+	private SistemasDeEstoque sistemasDeEstoque;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SistemaCadastroApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+
+		Produto produto1 = new Produto("Computador", 2000.0, 10);
+		Produto produto2 = new Produto("NarutoFigurie", 100.0, 20);
+
+		sistemasDeEstoque.adicionarprduto(produto1);
+		sistemasDeEstoque.adicionarprduto(produto2);
+
+		System.out.println("Lista de produtos:");
+		sistemasDeEstoque.listarProdutos();
+
+	}
 }
---
+++ sistema-cadastro/src/main/java/cadastro/com/sistema_cadastro/services/SistemasDeEstoque.java
@@ -0,0 +1,29 @@
+package cadastro.com.sistema_cadastro.services;
+
+import cadastro.com.sistema_cadastro.entities.Produto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class SistemasDeEstoque {
+
+    List<Produto> produtos = new ArrayList<>();
+
+    public void adicionarprduto(Produto produto) {
+        produtos.add(produto);
+    }
+    public List<Produto> listarProdutos() {
+        for (Produto produto : produtos) {
+            System.out.println("Nome: " + produto.getName());
+            System.out.println("Preço: " + produto.getPreco());
+            System.out.println("Quantidade em Estoque: " + produto.getQuantidadeEmEstoque());
+            System.out.println("------------------------------");
+        }
+        return produtos;
+    }
+
+
+}
